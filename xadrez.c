07849b0e395a9386a5d32fe04b89be825ee2a513
AlphSlayer <alphonsusguitar@gmail.com>
2025-04-01 00:30:10 +0000

    jogo de xadrez dificuldade novato

--- xadrez.c
+++ xadrez.c
@@ -1,32 +1,105 @@
 #include <stdio.h>
 
-// Desafio de Xadrez - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
-// O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
+//Declaração das variáveis e da posição inicial das peças
+int peca, i=0;
+int torre = 11, bispo = 31, rainha = 58;
 
-int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
+//A cada movimento vertical soma 10 ou subtrai 10
+//A cada movimento horizontal soma 1 ou subtrai 1
 
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+int main () {
 
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+    //Demonstração do Tabuleiro
+    printf("Posição inicial das peças:\n");
+    printf("   ---------------------------------------------------------------------------------\n");
+    printf("   |         |         |         |         |         |         |         |         |\n");
+    printf("80 |   81    |   82    |   83    |   84    |   85    |   86    |   87    |   88    |\n");
+    printf("   |         |         |         |         |         |         |         |         |\n");
+    printf("   ---------------------------------------------------------------------------------\n");
+    printf("   |         |         |         |         |         |         |         |         |\n");
+    printf("70 |   71    |   72    |   73    |   74    |   75    |   76    |   77    |   78    |\n");
+    printf("   |         |         |         |         |         |         |         |         |\n");
+    printf("   ---------------------------------------------------------------------------------\n");
+    printf("   |         |         |         |         |         |         |         |         |\n");
+    printf("60 |   61    |   62    |   63    |   64    |   65    |   66    |   67    |   68    |\n");
+    printf("   |         |         |         |         |         |         |         |         |\n");
+    printf("   ---------------------------------------------------------------------------------\n");
+    printf("   |         |         |         |         |         |         |         |  Rainha |\n");
+    printf("50 |   51    |   52    |   53    |   54    |   55    |   56    |   57    |   58    |\n");
+    printf("   |         |         |         |         |         |         |         |         |\n");
+    printf("   ---------------------------------------------------------------------------------\n");
+    printf("   |         |         |         |         |         |         |         |         |\n");
+    printf("40 |   41    |   42    |   43    |   44    |   45    |   46    |   47    |   48    |\n");
+    printf("   |         |         |         |         |         |         |         |         |\n");
+    printf("   ---------------------------------------------------------------------------------\n");
+    printf("   |  Bispo  |         |         |         |         |         |         |         |\n");
+    printf("30 |   31    |   32    |   33    |   34    |   35    |   36    |   37    |   38    |\n");
+    printf("   |         |         |         |         |         |         |         |         |\n");
+    printf("   ---------------------------------------------------------------------------------\n");
+    printf("   |         |         |         |         |         |         |         |         |\n");
+    printf("20 |   21    |   22    |   23    |   24    |   25    |   26    |   27    |   28    |\n");
+    printf("   |         |         |         |         |         |         |         |         |\n");
+    printf("   ---------------------------------------------------------------------------------\n");
+    printf("   |  Torre  |         |         |         |         |         |         |         |\n");
+    printf("10 |   11    |   12    |   13    |   14    |   15    |   16    |   17    |   18    |\n");
+    printf("   |         |         |         |         |         |         |         |         |\n");
+    printf("   ---------------------------------------------------------------------------------\n");
+    printf("   |    1    |    2    |    3    |    4    |    5    |    6    |    7    |    8    |\n");
 
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+    //Menu
+    printf("Selecione o movimento desejado:\n\n");
+    printf(" 1 - Torre - 5 casas para direita.\n");
+    printf(" 2 - Bispo - 5 casas na diagonal para cima e à direita\n");
+    printf(" 3 - Rainha - 7 casas para a esquerda.\n"); 
+    printf("Escolha: ");
+    scanf("%d",&peca);
 
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
 
-    // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
+    //Movimentação das peças
+    switch (peca)
+    {
+        case 1:
+            printf("A torre está na casa %d\n",torre);
+            while (i<5) // Movimento horizontal
+                {
+                torre = torre + 1;
+                printf("Direita\n");
+                i++;
+                }
+            printf("A torre moveu-se para a casa %d\n",torre);    
+            i=0;
+        break;
 
-    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
-    // Inclua o uso de continue e break dentro dos loops.
+        case 2:
+            printf("O Bispo está na casa %d\n",bispo);
+            for (i=0; i<5; i++)//movimento horizontal e vertical
+                {
+                bispo = bispo + 1;
+                bispo = bispo + 10;
+                printf("Direita e Cima\n");
+                }
+          
+            printf("O Bispo moveu-se para a casa casa %d",bispo);
+            i=0;
 
-    return 0;
+        break;
+
+        case 3:
+            printf("A Rainha está na casa %d\n",rainha);
+           do
+           {
+            rainha = rainha - 1;
+            printf("Esquerda\n");
+            i++;           } 
+            
+            while (i < 7); // Movimento horizontal
+                printf("A Rainha moveu-se para a casa %d\n",rainha);
+        break;
+    
+    default:
+            printf("Selecione um dos movimentos disponíveis!\n");
+        break;
+    }
+
+return 0;
 }
